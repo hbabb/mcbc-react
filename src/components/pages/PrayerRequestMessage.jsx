b059aa66e9e2785d99fe2122f2290796f92f315f
Heath Babb <hbabb@hbc-data.com>
2024-07-30 21:50:27 -0400

    feat: added PrayerRequestMessage for Prayer page

---
+++ src/components/pages/PrayerRequestMessage.jsx
@@ -0,0 +1,105 @@
+// mcbc-react/frontend/src/components/pages/PrayerRequestMessage.jsx
+
+import { useEffect, useState } from 'react'
+import useFetch from '../../hooks/useFetch'
+import { format } from 'date-fns'
+
+import '../../styles/components/PrayerRequestMessage.css'
+
+const renderRichText = (content) => {
+  if (!content) return null
+
+  return content.map((block, index) => {
+    const children = block.children
+      ? block.children.map((child) => child.text).join('')
+      : ''
+
+    switch (block.type) {
+      case 'paragraph':
+        return <p key={index}>{children}</p>
+      case 'heading':
+        switch (block.level) {
+          case 1:
+            return <h1 key={index}>{children}</h1>
+          case 2:
+            return <h2 key={index}>{children}</h2>
+          case 3:
+            return <h3 key={index}>{children}</h3>
+          default:
+            return <p key={index}>Unknown heading level</p>
+        }
+      case 'list':
+        return block.format === 'ordered' ? (
+          <ol key={index}>{children}</ol>
+        ) : (
+          <ul key={index}>{children}</ul>
+        )
+      case 'list-item':
+        return <li key={index}>{children}</li>
+      case 'link':
+        return (
+          <a key={index} href={block.url}>
+            {children}
+          </a>
+        )
+      case 'quote':
+        return <blockquote key={index}>{children}</blockquote>
+      case 'code':
+        return <pre key={index}>{children}</pre>
+      case 'image':
+        return (
+          <img
+            key={index}
+            src={block.image.url}
+            alt={block.image.alternativeText}
+            width={block.image.width}
+            height={block.image.height}
+          />
+        )
+      default:
+        return <p key={index}>Unknown block type</p>
+    }
+  })
+}
+
+const PrayerRequestMessage = () => {
+  const { data, isLoading, isError } = useFetch(
+    '/api/prayer-request-message?sort[0]=date:desc&pagination[limit]=1',
+  )
+  const [content, setContent] = useState(null)
+  const [title, setTitle] = useState('')
+
+  useEffect(() => {
+    if (data) {
+      console.log('Fetched data:', data) // Log the data to verify the structure
+      const richTextContent = data.data[0]?.attributes?.content
+      const messageTitle = data.data[0]?.attributes?.title
+      console.log('Rich Text Content:', richTextContent) // Log the structure of the content
+      setContent(richTextContent)
+      setTitle(messageTitle)
+    }
+  }, [data])
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (isError) {
+    return <p>Error: {isError.message}</p>
+  }
+
+  return (
+    <div className="prayer-request-message">
+      <h1>Message from the Prayer Team</h1>
+      {data?.data?.map((item) => (
+        <div key={item.id}>
+          <h2>{title}</h2>
+          {renderRichText(content)}
+          <p>published: {format(item.attributes.date, 'dd/MM/yyyy')}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default PrayerRequestMessage
--- src/pages/Prayer.jsx
+++ src/pages/Prayer.jsx
@@ -1,10 +1,8 @@
 // TODO: Add code here
-import LogoSection from '../components/pages/LogoSection'
 
 function Prayer() {
   return (
     <div className="page-container">
-      <LogoSection />
       <h1>Prayer</h1>
     </div>
   )
